6fc15ac427a7a89fabd7694f66d91d624a0e9a28
AlexRTC1997 <aturpoco@unsa.edu.pe>
2022-12-12 21:30:30 -0500

    UPDATE: App Database Schema

--- app/src/main/java/com/example/eb_project/db/DbHelper.java
+++ app/src/main/java/com/example/eb_project/db/DbHelper.java
@@ -10,7 +10,17 @@ public class DbHelper extends SQLiteOpenHelper {
 
     private static final int DB_VERSION = 1;
     private static final String DB_NAME = "articles.db";
-    public static final String TABLE_NAME = "articles";
+
+    public static final String ARTICLES_TABLE_NAME = "articles";
+    public static final String MEASUREMENT_TABLE_NAME = "measurement_units";
+    public static final String BRAND_TABLE_NAME = "brands";
+    public static final String STATUS_TABLE_NAME = "status";
+
+    private static final String STATUS_QUERY = "CREATE TABLE " + STATUS_TABLE_NAME + "(StaId TEXT NOT NULL, StaDes TEXT NOT NULL, StaSta TEXT NOT NULL, PRIMARY KEY(\"StaId\"))";
+    private static final String BRAND_QUERY = "CREATE TABLE " + BRAND_TABLE_NAME + "(BraId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, BraNam TEXT NOT NULL, BraSta TEXT NOT NULL, FOREIGN KEY(\"BraSta\") REFERENCES \"Status\"(\"StaId\"))";
+    private static final String MEASUREMENT_QUERY = "CREATE TABLE " + MEASUREMENT_TABLE_NAME + "(MeaUniId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, MeaUniNam TEXT NOT NULL, MeaUniSta TEXT NOT NULL, FOREIGN KEY(\"MeaUniSta\") REFERENCES \"Status\"(\"StaId\"))";
+    private static final String ARTICLES_QUERY = "CREATE TABLE " + ARTICLES_TABLE_NAME + "(ArtId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ArtNam TEXT NOT NULL, ArtMeaUni INTEGER NOT NULL, ArtUniPri REAL NOT NULL, ArtBra INTEGER NOT NULL, ArtSta TEXT NOT NULL, FOREIGN KEY(\"ArtBra\") REFERENCES \"Brands\"(\"BraId\"), FOREIGN KEY(\"ArtSta\") REFERENCES \"Status\"(\"StaId\"), FOREIGN KEY(\"ArtMeaUni\") REFERENCES \"Measurement Units\"(\"MeaUniId\"))";
+
 
     public DbHelper(@Nullable Context context) {
         super(context, DB_NAME, null, DB_VERSION);
@@ -18,19 +28,19 @@ public class DbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("CREATE TABLE " + TABLE_NAME + "(" +
-                "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                "name TEXT NOT NULL," +
-                "unit TEXT NOT NULL," +
-                "price INT NOT NULL," +
-                "brand TEXT NOT NULL," +
-                "status CHAR NOT NULL" +
-                ")");
+        sqLiteDatabase.execSQL(STATUS_QUERY);
+        sqLiteDatabase.execSQL(BRAND_QUERY);
+        sqLiteDatabase.execSQL(MEASUREMENT_QUERY);
+        sqLiteDatabase.execSQL(ARTICLES_QUERY);
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL("DROP TABLE " + TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE " + ARTICLES_TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE " + MEASUREMENT_TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE " + BRAND_TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE " + STATUS_TABLE_NAME);
         onCreate(sqLiteDatabase);
     }
 }
